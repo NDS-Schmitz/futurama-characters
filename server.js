100c645183275c11c23fcc24f24c964db75eaa5c
Nick Schmitz <nick.d.schmitz@gmail.com>
2023-01-12 09:41:39 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const { request } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//json data
+
+const characters = {
+	fry: {
+		'Full Name': 'Phillip. J Fry',
+		'Birth Date': 'August 14, 1974',
+		Speices: 'Human',
+		Planet: 'Earth',
+	},
+	bender: {
+		'Full Name': 'Bender Bending Rodríguez',
+		'Birth Date': '2996',
+		Speices: 'Robot',
+		Planet: 'Earth',
+	},
+	farnsworth: {
+		'Full Name': 'Hubert J. Farnsworth',
+		'Birth Date': 'April 9, 2841',
+		Speices: 'Human',
+		Planet: 'Earth',
+	},
+	leela: {
+		'Full Name': 'Turanga Leela',
+		'Birth Date': 'July 29, 2975',
+		Speices: 'Mutant',
+		Planet: 'Earth',
+	},
+	amy: {
+		'Full Name': 'Amy Wong-Kroker',
+		'Birth Date': 'December 2979',
+		Speices: 'Human',
+		Planet: 'Mars',
+	},
+	zoidberg: {
+		'Full Name': 'John A. Zoidberg',
+		'Birth Date': 'May 5th, 2914',
+		Speices: 'Decapodian',
+		Planet: 'Decapod 10',
+	},
+	unknown: {
+		Description:
+			'This character does not exist or was destroyed by Lrrr of the planet omicron persei 8.',
+	},
+};
+
+//ROUTERS
+app.get('/', (request, response) => {
+	response.sendFile(`${__dirname}/index.html`);
+});
+
+app.get('/api/:name', (request, response) => {
+	const charName = request.params.name.toLowerCase();
+	if (characters[charName]) {
+		response.json(characters[charName]);
+	} else {
+		response.json(characters['unknown']);
+	}
+});
+
+//server
+
+app.listen(PORT, (request, response) => {
+	console.log(
+		`The server is running on port: ${PORT} you should go catch it before it gets away`
+	);
+});
